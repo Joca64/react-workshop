57b55637d83341109fdfb2dd9fcae753f517004d
Joaquim Mendes <joaquim.mendes@INNOWAVE-458.local>
2018-10-01 17:30:09 +0100

    step 5

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import env from '@env';
 
-const App = () => 
-console.log({env}) ||
-(
-    <div>
-        <h1>Hello, whatever!</h1>
-    </div>
-);
+const App = () => {
+    console.log('env', env);
+    return (
+        <div className="container">
+            <div className="col-12">
+                <h1 className="text-center">I should be centered!</h1>
+            </div>
+        </div>
+    )
+};
 
 export default App;
