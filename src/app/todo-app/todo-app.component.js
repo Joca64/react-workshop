bcc54ce8029e78194f2aedbcaa895d8234372308
Joaquim Mendes <joaquim.mendes@INNOWAVE-458.local>
2018-10-02 15:41:07 +0100

    step 8 full

--- src/app/todo-app/todo-app.component.js
+++ src/app/todo-app/todo-app.component.js
@@ -1,66 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 
 import TodoList from './todo-list';
+import TodoAdder from './todo-adder';
 
+const TodoApp = () => (
+    <div>
+        <h1>ToDo works!</h1>
 
-class TodoApp extends Component {
+        <TodoAdder />
 
-    constructor(props) {
-        super();
-
-        this.state = {
-            newTodo: ''
-        }
-    }
-
-    handleToDoCheckBoxChange = (event, i) => {
-        const value = event.target.checked;
-
-        this.props.toggleTodo({index: i, value});
-    }
-
-    handleNewToDoChange = (event) => {
-        this.setState({newTodo: event.target.value});
-    }
-
-    addTodo = () => {
-        this.props.addTodo({name: this.state.newTodo});
-    }
-
-    render() {
-        return (
-            <div>
-                <h1>TodoApp works!</h1>
-
-                <div>
-                    <input
-                        type="text"
-                        value={this.state.newTodo}
-                        onChange={this.handleNewToDoChange}
-                    />
-                    <button onClick={this.addTodo}>Add ToDo</button>
-                </div>
-
-                <ul>
-                    {this.props.todoList.map((obj, i) => (
-                        <li key={i}>
-                            <input
-                                name={i}
-                                type="checkbox"
-                                checked={obj.done}
-                                onChange={ e => this.handleToDoCheckBoxChange(e, i) }
-                            />
-                            {obj.name}
-                        </li>)
-                    )}
-                </ul>
-
-                <hr />
-                <h2>Debug Stuff</h2>
-                <pre>{JSON.stringify(this.props.todoList, null, 2)}</pre>
-            </div>
-        );
-    }
-}
+        <TodoList />
+    </div>
+);
 
 export default TodoApp;
